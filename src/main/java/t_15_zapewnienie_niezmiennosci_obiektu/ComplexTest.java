4c1589eaff73b561e9e15ec18f1178929138875b
bakeraw2 <notarget1>
2014-04-25 09:53:57 +0200

    poprawki

--- src/main/java/t_15_zapewnienie_niezmiennosci_obiektu/ComplexTest.java
+++ src/main/java/t_15_zapewnienie_niezmiennosci_obiektu/ComplexTest.java
@@ -5,6 +5,9 @@ package t_15_zapewnienie_niezmiennosci_obiektu;
  */
 public class ComplexTest {
     public static void main(String[] args) {
+
+        // test
+
         Complex complex = new Complex(3.24F, 5.422F);
         System.out.println(complex);
     }
